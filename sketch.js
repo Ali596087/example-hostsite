2ebb1d2f617181ee48b49fd5111ae63fa13e2817
Ali596087 <31707227+Ali596087@users.noreply.github.com>
2018-10-01 14:51:27 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+	fill(0,0,0)
+	ellipse(mouseX -5, mouseY - 5,10,10);
+}
+html, body {
+  margin: 0;
+  padding: 0;
+}
